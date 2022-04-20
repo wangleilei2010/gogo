3afe5cc40e1f4fb1caf6c9bb855797629b45a2b7
汪磊磊 <wangleilei@geekplus.com.cn>
2022-04-20 15:48:54 +0800

    http优化

--- http/session.go
+++ http/session.go
@@ -9,14 +9,16 @@ import (
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"time"
 )
 
-type Response struct {
+type Response[T any] struct {
 	Text       string
 	StatusCode int
+	Value      T
 }
 
-func (resp *Response) Json() map[string]interface{} {
+func (resp *Response[T]) Json() map[string]interface{} {
 	var mapData map[string]interface{}
 	if err := json.Unmarshal([]byte(resp.Text), &mapData); err != nil {
 		return nil
@@ -24,7 +26,7 @@ func (resp *Response) Json() map[string]interface{} {
 	return mapData
 }
 
-func (resp *Response) J(expression string) interface{} {
+func (resp *Response[T]) J(expression string) interface{} {
 	if j := resp.Json(); j != nil {
 		if r, e := jsonpath.JsonPathLookup(j, expression); e != nil {
 			return nil
@@ -40,12 +42,14 @@ type Session struct {
 	core *http.Client
 }
 
-func (client *Session) request(url, method, body string) (*Response, error) {
-	var req *http.Request
-	var res *http.Response
-	var err error
-	var respTextBytes []byte
-	var bodyReader io.Reader = nil
+func request[T any](client *Session, url, method, body string) (*Response[T], error) {
+	var (
+		req           *http.Request
+		res           *http.Response
+		err           error
+		respTextBytes []byte
+		bodyReader    io.Reader = nil
+	)
 
 	if body != "" {
 		var bodyBytes = []byte(body)
@@ -65,17 +69,34 @@ func (client *Session) request(url, method, body string) (*Response, error) {
 	if respTextBytes, err = ioutil.ReadAll(res.Body); err != nil {
 		return nil, err
 	} else {
-		return &Response{Text: string(respTextBytes),
+		var value T
+		err = json.Unmarshal(respTextBytes, &value)
+		if err != nil {
+			return nil, err
+		}
+		return &Response[T]{Text: string(respTextBytes), Value: value,
 			StatusCode: res.StatusCode}, nil
 	}
 }
 
-func (client *Session) Post(url, body string) (*Response, error) {
-	return client.request(url, "POST", body)
+func Post[T any](client *Session, url, body string) (*Response[T], error) {
+	var session *Session
+	if client == nil {
+		session = NewSession(time.Second * 120)
+	} else {
+		session = client
+	}
+	return request[T](session, url, "POST", body)
 }
 
-func (client *Session) Get(url string) (*Response, error) {
-	return client.request(url, "GET", "")
+func Get[T any](client *Session, url string) (*Response[T], error) {
+	var session *Session
+	if client == nil {
+		session = NewSession(time.Second * 120)
+	} else {
+		session = client
+	}
+	return request[T](session, url, "GET", "")
 }
 
 type MyTransport struct {
@@ -94,15 +115,18 @@ func (t *MyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return t.transport().RoundTrip(req)
 }
 
-func NewSession() *Session {
+func NewSession(timeOut time.Duration) *Session {
 	t := &MyTransport{}
 	jar, err := cookiejar.New(nil)
 	if nil != err {
 		log.Fatal(err)
 	}
-	client := http.DefaultClient
+	client := &http.Client{}
 	client.Transport = t
 	client.Jar = jar
+	if timeOut != 0 {
+		client.Timeout = timeOut
+	}
 	return &Session{
 		core: client,
 	}
